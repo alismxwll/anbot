1e304d0a49c3e718fc3438c27a1537f581f699f8
Andrew <andrew@Kristys-MacBook-Pro-2.local>
2015-04-23 14:52:04 -0700

    add sites_controller create method

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,4 +3,20 @@ class SitesController < ApplicationController
   def new
     @site = Site.new
   end
+
+  def create
+    @site = Site.create(site_params)
+    if @site.valid?
+      flash[:notice] = 'Site should be deploying, if you run into any issues, contact us!'
+      redirect_to site_path @site
+    else
+      flash[:alert] = 'The site could not be created!'
+      render 'new'
+    end
+  end
+
+  private
+  def site_params
+    params.require(:site).permit(:user_id, :title).merge(:user_id => current_user.id)
+  end
 end
